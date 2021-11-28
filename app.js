b3365cf718fd1e4cd7dd07823cb026ba548a7156
kumar <santanupaikaray23@gmail.com>
2021-11-28 14:47:43 +0530

    add all files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const app=express()
+const cors = require("cors")
+const paymentRoute=require('./paymentRoute')
+const bodyParser=require("body-parser")
+
+const port=5000
+app.use(cors())
+app.use(bodyParser.json())
+app.use("/api",paymentRoute);
+app.listen(port,()=>{
+    console.log(`App is running at ${port}`)
+})
